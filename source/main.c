49f790abd138b1f5e1daf72b908b07e6bf35481a
vishshukla <vwshukla@gmail.com>
2020-03-05 02:48:58 -0800

    build upon 2

--- source/main.c
+++ source/main.c
@@ -25,6 +25,7 @@
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
+unsigned char highscore = 0x00;
 
 void TimerOn() {
 	TCCR1B = 0X0B;
@@ -60,6 +61,33 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
+void set_PWM(double f) {
+    static double current_f;
+    if (f != current_f) {
+        if(!f) TCCR3B &= 0x08;
+        else TCCR3B |= 0x03;
+        
+        if(f < 0.954) OCR3A = 0xFFFF;
+        else if (f > 31250) OCR3A = 0x0000;
+        else OCR3A = (short)(8000000 / (128 * f)) - 1;
+
+        TCNT3 = 0;
+        current_f = f;
+    }
+}
+void PWM_on() {
+    TCCR3A = (1 << COM3A0);
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+
 
 enum GlobalSM {MainMenu, ReflexGame, FollowPattern} GameState;
 
@@ -68,6 +96,10 @@ enum MainMenuSM {mainmenu_start, mainmenu_select, mainmenu_reflex, mainmenu_patt
 void MainMenuTick() {
     switch(mainmenu_state) {
         case mainmenu_start:
+            PORTB = 0x00;
+            char greeting[20];
+            sprintf(greeting,"Main Menu, Score: %d",highscore);
+            LCD_DisplayString(1,greeting);
             mainmenu_state = mainmenu_select;
             break;
         case mainmenu_select:
@@ -88,16 +120,26 @@ unsigned char current_reflex_pos;
 unsigned char reflex_temp;
 
 
+// RNG FROM SCRATH
 void ReflexSetup(unsigned char reflex_pattern[]) {
     for(unsigned int i = 0; i < REFLEX_SIZE; ++i) {
-        reflex_pattern[i] = rand() % 3;
+        reflex_pattern[i] = (int)get_rand() % 3;
     }
+}
+
+unsigned short lfsr = 0xACE1u;
+int bit;
+
+  int get_rand() {
+    bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+    return lfsr =  (lfsr >> 1) | (bit << 15);
 }
 
 enum ReflexSM {reflex_start, reflex_on, reflex_off} reflex_state;
 void ReflexTick() {
     switch(reflex_state) { //transitions
         case reflex_start:
+            ReflexSetup(reflex_pattern); 
             reflex_state = reflex_on;
             break;
         case reflex_on:
@@ -113,77 +155,43 @@ void ReflexTick() {
 
     switch(reflex_state) { //actions
         case reflex_start:
+        // CALL THIS WHEN USER HITS OPTION IN MENU
             break;
         case reflex_on:
-            reflex_temp = (0x01 << reflex_pattern[current_reflex_pos]);
+            PORTB = (0x01 << reflex_pattern[current_reflex_pos]);
             current_reflex_pos = (current_reflex_pos + 1) % REFLEX_SIZE;
             break;
         case reflex_off:
-            reflex_temp = 0x00;
+            PORTB = 0x00;
         default:
             break;
     }
-
 }
 
-unsigned char speaker_temp = 0x00;
-enum SpeakerSM {speaker_start, speaker_s0, speaker_s1, speaker_wait} Speaker_state;
-bool hitRight = true;
-void SpeakerSM_Tick() {
-    switch(Speaker_state) {
-        case speaker_start:
-            Speaker_state = speaker_s0;
-            break;
-        case speaker_s0:
-            if ((~PINA & 0x07) == ((0x01 << reflex_pattern[(current_reflex_pos - 1) % REFLEX_SIZE ]))) Speaker_state = speaker_s1;
-            //else hitRight = false;
-            break;
-        case speaker_s1:
-            if (reflex_temp == 0x00) Speaker_state = speaker_wait;
-            break;
-        case speaker_wait:
-            if (reflex_temp != 0x00) Speaker_state = speaker_s0;
-            break;
-        default: break;
+enum ReflexLCD {reflexlcd_s0 } reflexlcd_state;
+
+void ReflexLCDTick() {
+    char score[5];
+    if ((~PINA & 0x07) == ((0x01 << reflex_pattern[(current_reflex_pos - 1) % REFLEX_SIZE ]))) {
+        highscore++;
+        set_PWM(261.23);
+    } else if ((~PINA & 0x07) != 0x00) {
+        highscore--;
+        set_PWM(100.00);
+    } else {
+        set_PWM(0);
     }
-
-    switch(Speaker_state) {
-        case speaker_start:
-        case speaker_s0: 
-			speaker_temp = 0x00; 
-			break;
-        case speaker_s1:
-            speaker_temp = !speaker_temp;   
-			break;
-        case speaker_wait:
-            speaker_temp = 0x00;
-            break;
-        default: break;
-    }
+    sprintf(score,"%d",highscore);
+    LCD_DisplayString(1,score);
 }
-
-
-enum SpeakerReflexSM {speakerreflex_state} SpeakerReflex_state;
-
-void SpeakerReflexSMsTick() {
-	switch(SpeakerReflex_state) { // transitions
-		case speakerreflex_state: 
-		default: break;
-	}
-
-	switch(SpeakerReflex_state) { // actions
-		case speakerreflex_state: PORTB = reflex_temp | (speaker_temp << 4);
-                 break;
-		default: break;
-	}
-}
-
-
 
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xFF; PORTC = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
     srand(time(0));
+    
     
     const unsigned long timerPeriod = 3;
     TimerOn();
@@ -191,10 +199,12 @@ int main(void) {
 
     GameState = MainMenu;
 
-    reflex_state = reflex_on;
+    reflex_state = reflex_start;
     unsigned long Reflex_elapsedTime = 0;
-    unsigned long Reflex_timer = 1000;
-    ReflexSetup(reflex_pattern); // CALL THIS WHEN USER HITS OPTION IN MENU
+    unsigned long Reflex_timer = 500;
+
+    PWM_on();
+	LCD_init();
     while (1) {
         switch(GameState) { //actions
             case MainMenu:
@@ -203,20 +213,16 @@ int main(void) {
             case ReflexGame:
                 if(Reflex_elapsedTime >= Reflex_timer) {
                     ReflexTick();
+                    ReflexLCDTick();
                     Reflex_elapsedTime = 0;
-                    Reflex_timer -= 50;
                 }
                 if((~PINA & 0x04) || Reflex_timer < 200 ) { //reset
-                    PORTB = 0x00;
                     reflex_state = reflex_start;
                     GameState = MainMenu;
+                    mainmenu_state = mainmenu_start;
                     Reflex_elapsedTime = 0;
-                    Reflex_timer = 1000;
-                    hitRight = true;
-                    break;
+                    Reflex_timer = 500;
                 }
-                SpeakerSM_Tick();
-                SpeakerReflexSMsTick();
                 Reflex_elapsedTime+=timerPeriod;
                 break;
             case FollowPattern: //TODO
