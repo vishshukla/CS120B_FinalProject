e10512c30a29751a86e7c7d704dc536f8ef18dca
vishshukla <vwshukla@gmail.com>
2020-03-12 14:47:29 -0700

    done w/ beta

--- source/main.c
+++ source/main.c
@@ -21,11 +21,16 @@
 
 // GLOBAL DEFAULTS
 #define REFLEX_SIZE 10
+#define PATTERN_SIZE 6
+#define POLY_MASK_32 0xB4BCD35C
+#define POLY_MASK_31 0x7A5BC2E3
 
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
-unsigned char highscore = 0x00;
+unsigned long reflex_highscore = 500;
+unsigned long pattern_highscore = 0;
+unsigned long Reflex_timer = 500;
 
 void TimerOn() {
 	TCCR1B = 0X0B;
@@ -75,6 +80,7 @@ void set_PWM(double f) {
         current_f = f;
     }
 }
+
 void PWM_on() {
     TCCR3A = (1 << COM3A0);
     TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
@@ -88,52 +94,109 @@ void PWM_off() {
 }
 
 
+//////////////////// JOYSTICK ////////////////////
 
-enum GlobalSM {MainMenu, ReflexGame, FollowPattern} GameState;
+unsigned short ADCValue;
+unsigned const short joyStickUpValue = 700;
+unsigned const short joyStickDownValue = 300;
 
-enum MainMenuSM {mainmenu_start, mainmenu_select, mainmenu_reflex, mainmenu_pattern, mainmenu_hold} mainmenu_state;
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
 
+bool joyStickUp() {
+    ADCValue = ADC;
+    return (ADCValue > joyStickUpValue) ? true : false;
+}
+
+bool joyStickDown() {
+    ADCValue = ADC;
+    return (ADCValue < joyStickDownValue) ? true : false;
+}
+
+enum GlobalSM {MainMenu, ReflexGame, PatternGame} GameState;
+char displayReflex[20];
+char displayPattern[20];
+enum MainMenuSM {mainmenu_reset,mainmenu_start, mainmenu_select,mainmenu_pressed, mainmenu_hold} mainmenu_state;
+unsigned short type = 0;
 void MainMenuTick() {
     switch(mainmenu_state) {
-        case mainmenu_start:
+        case mainmenu_reset:
             PORTB = 0x00;
-            char greeting[20];
-            sprintf(greeting,"Main Menu, Score: %d",highscore);
-            LCD_DisplayString(1,greeting);
-            mainmenu_state = mainmenu_select;
+            if((~PINA & 0x0E) == 0x00) mainmenu_state = mainmenu_start;
             break;
-        case mainmenu_select:
-            if(((~PINA & 0x0F) == 0x01)) {
-                mainmenu_state = mainmenu_hold;
+        case mainmenu_start:
+            set_PWM(0);
+            sprintf(displayReflex, "Reflex: %d", reflex_highscore);
+            LCD_DisplayString(1,displayReflex);
+            sprintf(displayPattern, "Pattern: %d", pattern_highscore);
+            LCD_Cursor(17);
+            // only for printing
+            for(unsigned char i = 0; displayPattern[i] != '\0'; ++i) {
+                LCD_WriteData(displayPattern[i]);
             }
+            mainmenu_state = mainmenu_select;
+            break; 
+        case mainmenu_select:
+            LCD_Cursor(1);
+                if (((~PINA) & 0x0E) == 0x04) {
+                    type = 1;
+                    mainmenu_state = mainmenu_hold;
+                }
+                 if (((~PINA) & 0x0E) == 0x02) {
+                     type = 0;
+                     mainmenu_state = mainmenu_hold;
+                 }
             break;
         case mainmenu_hold:
-            if((~PINA & 0x0F) == 0x00) {
+            if (((~PINA) & 0x0E) == 0x00) {
+                GameState = (type) ? PatternGame : ReflexGame;
                 mainmenu_state = mainmenu_start;
-                GameState = ReflexGame;// change gamemode
             }
+            break;
     }
+
 }
 
 unsigned char reflex_pattern[REFLEX_SIZE];
 unsigned char current_reflex_pos;
-unsigned char reflex_temp;
 
 
-// RNG FROM SCRATH
 void ReflexSetup(unsigned char reflex_pattern[]) {
     for(unsigned int i = 0; i < REFLEX_SIZE; ++i) {
         reflex_pattern[i] = (int)get_rand() % 3;
     }
 }
 
-unsigned short lfsr = 0xACE1u;
+unsigned short lfsr = 0xABF1u;
 int bit;
 
+// RNG FROM SCRATH
   int get_rand() {
     bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
     return lfsr =  (lfsr >> 1) | (bit << 15);
 }
+
+// int lfsr32, lfsr31;
+
+// int shift_lfsr(int *lfsr, int polynominal_mask) {
+//     int feedback;
+//     feedback = *lfsr & 1;
+//     *lfsr >>= 1;
+//     if (feedback == 1)
+//         *lfsr ^= polynominal_mask;
+//     return *lfsr;
+// }
+
+// void init_lfsrs(void) {
+//     lfsr32 = 0xABCDE;
+//     lfsr31 = 0x23456789;
+// }
+
+// int get_rand(void) {
+//     shift_lfsr(&lfsr32, POLY_MASK_32);
+//     return (shift_lfsr(&lfsr32,POLY_MASK_32) ^ shift_lfsr(&lfsr31, POLY_MASK_31)) & 0xffff;
+// }
 
 enum ReflexSM {reflex_start, reflex_on, reflex_off} reflex_state;
 void ReflexTick() {
@@ -171,42 +234,207 @@ void ReflexTick() {
 enum ReflexLCD {reflexlcd_s0 } reflexlcd_state;
 
 void ReflexLCDTick() {
-    char score[5];
-    if ((~PINA & 0x07) == ((0x01 << reflex_pattern[(current_reflex_pos - 1) % REFLEX_SIZE ]))) {
-        highscore++;
-        set_PWM(261.23);
-    } else if ((~PINA & 0x07) != 0x00) {
-        highscore--;
-        set_PWM(100.00);
+    char score[30];
+    sprintf(score,"Reflex Time: %d",Reflex_timer);
+    if ((~PINA & 0x0E) == ((0x01 << (reflex_pattern[(current_reflex_pos - 1) % REFLEX_SIZE ])))) {
+    } else if ((~PINA & 0x0E) != 0x00) {
     } else {
         set_PWM(0);
     }
-    sprintf(score,"%d",highscore);
     LCD_DisplayString(1,score);
 }
 
+bool exitReflexGame = false;
+enum UpdateReflexScore {updatereflexscore_start, updatereflexscore_press, updatereflexscore_hold } updatereflexscore_state;
+
+void UpdateReflexScoreTick() {
+    switch(updatereflexscore_state) { //transitions
+        case updatereflexscore_start:
+            if((~PINA & 0x0E) != 0x00) {
+                updatereflexscore_state = updatereflexscore_press;
+            }
+            break;
+        case updatereflexscore_press:
+            updatereflexscore_state = updatereflexscore_hold;
+            break;
+        case updatereflexscore_hold:
+            if((~PINA & 0x0E) == 0x00) updatereflexscore_state = updatereflexscore_start;
+            break;
+    }
+    switch(updatereflexscore_state) {
+        case updatereflexscore_press:
+            if ((~PINA & 0x0E) == (PORTB << 1)) {
+                Reflex_timer = Reflex_timer - 10;
+                set_PWM(261.23);
+            } else {
+                reflex_highscore = (Reflex_timer < reflex_highscore) ? Reflex_timer : reflex_highscore;
+                set_PWM(100.00);
+                exitReflexGame = true;
+            }
+            break;
+        case updatereflexscore_hold:
+            break;
+        default: break;
+    }
+}
+
+bool exitPatternGame = false;
+unsigned char* patterngame_pattern[PATTERN_SIZE];
+unsigned char current_pattern_pos = 0;
+
+
+void PatternSetup(char patterngame_pattern[]) {
+    for(unsigned int i = 0; i < PATTERN_SIZE; ++i) {
+        patterngame_pattern[i] = ((int)get_rand() % 2);
+    }
+}
+
+bool pattern_done = false;
+bool checkpattern_done = false;
+bool pattern_game_lost = false;
+enum PatternSM { pattern_start, pattern_on, pattern_off} pattern_state;
+char tempmsg[20];
+unsigned char checkpattern_pos = 0x00;
+
+void PatternTick() {
+    switch(pattern_state) { //transitions
+        case pattern_start:
+            PatternSetup(patterngame_pattern); 
+            pattern_done = false;
+            pattern_state = pattern_on;
+            current_pattern_pos = 0x00;
+            break;
+        case pattern_on:
+            pattern_state = pattern_off;
+            break;
+        case pattern_off:
+            if(current_pattern_pos < PATTERN_SIZE) {
+                pattern_state = pattern_on;
+            } else {
+                pattern_done = true;
+                if(checkpattern_done) {
+                    pattern_state = pattern_start;
+                    checkpattern_pos = 0x00;
+                    current_pattern_pos = 0x00;
+                    checkpattern_done = false;
+                }
+            }
+            break;
+        default:
+            pattern_state = pattern_on;
+            break;
+    }
+
+    switch(pattern_state) { //actions
+        case pattern_start:
+            break;
+        case pattern_on:
+            if(patterngame_pattern[current_pattern_pos]) {
+                LCD_DisplayString(1, "RIGHT");
+            } else {
+                LCD_DisplayString(1, "LEFT");
+            }
+            current_pattern_pos++;
+            break;
+        case pattern_off:
+            LCD_ClearScreen();
+            break;
+        default:
+            break;
+    }
+}
+
+unsigned long Pattern_timer = 600;
+unsigned long pattern_check_timer = 0;
+
+enum CheckPatternSM {checkpattern_start, checkpattern_move, checkpattern_hold} checkpattern_state;
+void CheckPatternTick() {
+    switch (checkpattern_state) {
+        case checkpattern_start:
+            if((short)ADC < 100 || (short)ADC > 800) {
+                checkpattern_state = checkpattern_move;
+            }
+            break;
+        case checkpattern_move:
+            checkpattern_state = checkpattern_hold;
+            break;
+        case checkpattern_hold:
+            if(!((short)ADC < 100 || (short)ADC > 800)) checkpattern_state = checkpattern_start;
+            break;
+    }
+    switch (checkpattern_state) {
+        case checkpattern_start: set_PWM(0); break;
+        case checkpattern_move: 
+            if( ( (short)ADC < 100 && (patterngame_pattern[checkpattern_pos] == 0x01) ) || ( (short)ADC > 600 && (patterngame_pattern[checkpattern_pos] == 0x00) )) {
+                pattern_game_lost = true;
+                LCD_DisplayString(1, "lol, trash");
+                set_PWM(100);
+            } else {
+                checkpattern_pos++;
+                set_PWM(261.23);
+            }
+            break;
+        case checkpattern_hold: 
+            if(checkpattern_pos == current_pattern_pos) {
+                checkpattern_done = true;
+                pattern_done = false;
+                checkpattern_state = checkpattern_start;
+                sprintf(tempmsg, "NICE! TIME: %d", pattern_check_timer);
+                pattern_highscore = ((pattern_highscore > pattern_check_timer) || pattern_highscore == 0) ? pattern_check_timer : pattern_highscore;
+                pattern_check_timer = 0x00;
+                LCD_DisplayString(1,tempmsg);
+            }
+            break;
+    }
+}
+
+unsigned long Reflex_elapsedTime = 0;
+
+unsigned long Pattern_elapsedTime = 0;
+
+void Reset() {
+    reflex_state = reflex_start;
+    pattern_state = pattern_start;
+    mainmenu_state = mainmenu_start;
+    checkpattern_state = checkpattern_start;
+    GameState = MainMenu;
+    mainmenu_state = mainmenu_reset;
+    Reflex_elapsedTime = 0;
+    pattern_check_timer = 0;
+    checkpattern_pos = 0x00;
+    current_pattern_pos = 0x00;
+    Reflex_timer = 500;
+    exitReflexGame = false;
+    exitPatternGame = false;
+    pattern_game_lost = false;
+    checkpattern_done = false;
+    pattern_done = false;
+    type = 0;
+}
+
+
 int main(void) {
-    DDRA = 0x00; PORTA = 0xFF;
+    DDRA = 0x00; PORTA = 0xFE;
     DDRB = 0xFF; PORTB = 0x00;
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
-    srand(time(0));
+   // init_lfsrs();    
     
     
-    const unsigned long timerPeriod = 3;
+    const unsigned long timerPeriod = 5;
     TimerOn();
     TimerSet(timerPeriod);
 
     GameState = MainMenu;
-
-    reflex_state = reflex_start;
-    unsigned long Reflex_elapsedTime = 0;
-    unsigned long Reflex_timer = 500;
+    mainmenu_state = mainmenu_reset;
+    long PatternLostMsg_elapsedTime = 0;
 
     PWM_on();
 	LCD_init();
+    ADC_init();
     while (1) {
-        switch(GameState) { //actions
+        get_rand();
+        switch(GameState) {
             case MainMenu:
                 MainMenuTick();
                 break;
@@ -216,16 +444,32 @@ int main(void) {
                     ReflexLCDTick();
                     Reflex_elapsedTime = 0;
                 }
-                if((~PINA & 0x04) || Reflex_timer < 200 ) { //reset
-                    reflex_state = reflex_start;
-                    GameState = MainMenu;
-                    mainmenu_state = mainmenu_start;
-                    Reflex_elapsedTime = 0;
-                    Reflex_timer = 500;
+                UpdateReflexScoreTick();
+                if((~PINA & 0x08) || (exitReflexGame)) { //reset
+                    Reset();
                 }
                 Reflex_elapsedTime+=timerPeriod;
                 break;
-            case FollowPattern: //TODO
+            case PatternGame:
+                if(Pattern_elapsedTime >= Pattern_timer) {
+                    PatternTick();
+                    Pattern_elapsedTime = 0;
+                }
+                if((pattern_done)) {
+                    pattern_check_timer+=timerPeriod;
+                    CheckPatternTick();
+                }
+                if(pattern_game_lost) {
+                    if(PatternLostMsg_elapsedTime > 500) {
+                        Reset();
+                        PatternLostMsg_elapsedTime = 0;
+                    }
+                    PatternLostMsg_elapsedTime += timerPeriod;
+                }
+                if((~PINA & 0x08)) { //reset
+                    Reset();
+                }
+                Pattern_elapsedTime +=timerPeriod;
                 break;
         }
         while (!TimerFlag) {}
